dd222081c1672bf533e4f1589094a92e72f0555f
Sven Reinck <sreinck@fluxparticle.de>
2025-07-05 11:00:07 +0200

    Aufgabe: Liste erstellen

--- src/main/java/com/example/contacts/controller/HelloBean.java
+++ src/main/java/com/example/contacts/controller/HelloBean.java
@@ -3,13 +3,27 @@ package com.example.contacts.controller;
 import jakarta.enterprise.context.*;
 import jakarta.inject.*;
 
+import java.util.*;
+
 @Named
 @RequestScoped
 public class HelloBean {
 
     private String name = "World";
 
+    private List<String> names = new ArrayList<>();
+
+    // actions
+    public void submit() {
+        if (name != null && !name.isEmpty()) {
+            names.add(name);
+        }
+    }
+
     // getter/setter
+    public List<String> getNames() {
+        return names;
+    }
 
     public String getName() {
         return name;
